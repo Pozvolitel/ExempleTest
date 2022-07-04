3a000d6af32a8728e5481535d9d9175826719313
Pozvolitel <108138316+Pozvolitel@users.noreply.github.com>
2022-07-04 13:08:36 +0300

    new

--- ExempleTest/Program.cs
+++ ExempleTest/Program.cs
@@ -25,32 +25,36 @@ namespace ExempleTest
         }
 
         //4. The user enters 2 numbers (A and B). Print to the console the result of dividing A by B and the division remainder.
-        static void Task4(ref int a, ref int b)
+        static (int dividingResult, int divisionResult) Task4(in int a, in int b)
         {
-           int dividing = a / b;
-           int division = a % b;           
+            int dividing = a / b;
+            int division = a % b;
+
+            return (dividing, division);
         }
 
         //5. The user enters 3 non-zero numbers (A, B and C). Print to the console the solution (X value) of the standard linear equation, where A*X+B=C.
-        static void Task5(ref float a, ref float b, ref float c)
-        {           
-            double x = Math.Ceiling((c - b) / a);            
+        static float Task5(float a, float b, float c)
+        {
+            return (float)Math.Round((c - b) / a, 5);
         }
 
         //6. The user enters 4 numbers (X1, Y1, X2, Y2) describing the coordinates of 2 points on the coordinate plane. Output the equation of a straight line in the format Y=AX+B passing through these points.
-        static void Task6(ref float y1, ref float y2, ref float x1, ref float x2)
+        static (float a, float b) Task6(float y1, float y2, float x1, float x2)
         {
             float a = (y1 - y2) / (x1 - x2);
-            float b = y2 - a * x2;         
+            float b = y2 - a * x2;
+
+            return (a, b);
         }
 
         //7. The variable N stores a natural two-digit number (10-99). Write a program that calculates and displays the sum of the digits of n.
-        static int Task7(ref int n)
-        {            
+         static int Task7(int n)
+        {
             int first = n / 10;
             int second = n % 10;
 
-            return first + second;            
+            return first + second;
         }
 
             static void Main(string[] args)
